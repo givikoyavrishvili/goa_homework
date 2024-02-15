8fd20353b5da32e4ec62ea8066bdfb0cc17c045a
givikoyavrishvili <givikoyavrishvili@gmail.com>
2024-02-15 21:14:49 +0200

    fiveth commit

---
+++ day 5/day_5.py
@@ -0,0 +1,24 @@
+my name is(givi)
+print(givi)
+
+print  username b
+username=yavrishvili
+print(username)
+
+print age
+age=13
+print(13)
+      
+  goa best in the world
+goa=goa best in the world
+print(goa)
+
+  goa people best
+goapeople=goa people  best
+print(goa)
+
+favorite= ticher batoni nika 
+print(favorite)
+
+favorite ticher 2
+print(favorite)
